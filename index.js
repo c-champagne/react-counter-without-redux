9edd83b51fda0bf3407bce44166a9ed1c26644bd
c-champagne <cassielchampagne@gmail.com>
2020-06-09 18:33:21 -0400

    Counter done?

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class Counter extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+    handleClick() {
+        this.setState({value: this.state.value + 1});
+
+    }
+    render() {
+        return <div>
+            <h1>Total: {this.state.value}</h1>
+            <button onClick={this.handleClick.bind(this)}>Click to Count</button>
+        </div>
+    }
+}
+
+
+
+
+
+ReactDOM.render(<Counter/>, document.getElementById('root'))
